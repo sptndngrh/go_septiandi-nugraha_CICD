af2d39f4eaa6c6daabfc9c12b668cbe17dfff527
sptndngrh <septiandin92@gmail.com>
2023-10-11 10:46:59 +0700

    Menambah repositori baru untuk tugas CI/CD

--- app/config/config.go
+++ app/config/config.go
@@ -36,15 +36,16 @@ func loadConfig() *AppConfig {
 
 	if err != nil {
 		logrus.Error("Config : Cannot load config file,", err.Error())
-		return nil
 	}
 
+	// var isRead = false
 	if val, found := os.LookupEnv("SERVERPORT"); found {
 		port, err := strconv.Atoi(val)
 		if err != nil {
 			logrus.Error("Config : invalid port value,", err.Error())
 			return nil
 		}
+		// isRead = true
 		res.SERVERPORT = port
 	}
 
@@ -54,22 +55,27 @@ func loadConfig() *AppConfig {
 			logrus.Error("Config : invalid db port value,", err.Error())
 			return nil
 		}
+		// isRead = true
 		res.DBPORT = port
 	}
 
 	if val, found := os.LookupEnv("DBHOST"); found {
+		// isRead = true
 		res.DBHOST = val
 	}
 
 	if val, found := os.LookupEnv("DBUSER"); found {
+		// isRead = true
 		res.DBUSER = val
 	}
 
 	if val, found := os.LookupEnv("DBPASS"); found {
+		// isRead = true
 		res.DBPASS = val
 	}
 
 	if val, found := os.LookupEnv("DBNAME"); found {
+		// isRead = true
 		res.DBNAME = val
 	}
 
